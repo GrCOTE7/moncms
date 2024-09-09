ec612808de572f69902d2d192ac0a2d14cab7558
GrCOTE7 <grcote7@gmail.com>
2024-09-09 10:29:37 +0200

    Fix: Good call of app name for title

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -1,46 +1,48 @@
 <?php
 
+/**
+ * (ɔ) Mon CMS - 2024-2024
+ */
+
 namespace App\View\Components;
 
-use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class AppBrand extends Component
 {
-    // public $appName = env('APP_NAME');
-    public $appName;
+	public $appName;
 
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        $this->appName = env('APP_NAME');
-     }
+	/**
+	 * Create a new component instance.
+	 */
+	public function __construct()
+	{
+		$this->appName = config('app.name');
+	}
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
-    {
-        return <<<'HTML'
-                <a href="/" wire:navigate>
-                    <!-- Hidden when collapsed -->
-                    <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
-                        <div class="flex items-center gap-2">
-                            <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
-                            <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
-                                {{ $appName }}
-                            </span>
-                        </div>
-                    </div>
+	/**
+	 * Get the view / contents that represent the component.
+	 */
+	public function render(): View|\Closure|string
+	{
+		return <<<'HTML'
+			    <a href="/" wire:navigate>
+			        <!-- Hidden when collapsed -->
+			        <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
+			            <div class="flex items-center gap-2">
+			                <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+			                <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
+			                    {{ $appName }}
+			                </span>
+			            </div>
+			        </div>
 
-                    <!-- Display when collapsed -->
-                    <div class="display-when-collapsed hidden mx-5 mt-4 lg:mb-6 h-[28px]">
-                        <x-icon name="s-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
-                    </div>
-                </a>
-            HTML;
-    }
+			        <!-- Display when collapsed -->
+			        <div class="display-when-collapsed hidden mx-5 mt-4 lg:mb-6 h-[28px]">
+			            <x-icon name="s-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
+			        </div>
+			    </a>
+			HTML;
+	}
 }
