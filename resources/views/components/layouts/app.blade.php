352929169fe0f6ae0958c2e2c1abfdeef44d6283
GrCOTE7 <grcote7@gmail.com>
2024-09-09 10:49:25 +0200

    Feat: Change collapse for menu

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
@@ -13,12 +13,12 @@
 <body class="min-h-screen font-sans antialiased bg-base-200/50 dark:bg-base-200">
 
     {{-- NAVBAR mobile only --}}
-    <x-nav sticky class="lg:hidden">
+    <x-nav sticky class="sm:hidden">
         <x-slot:brand>
             <x-app-brand />
         </x-slot:brand>
         <x-slot:actions>
-            <label for="main-drawer" class="lg:hidden me-3">
+            <label for="main-drawer" class="sm:hidden me-3">
                 <x-icon name="o-bars-3" class="cursor-pointer" />
             </label>
         </x-slot:actions>
@@ -27,7 +27,7 @@
     {{-- MAIN --}}
     <x-main full-width>
         {{-- SIDEBAR --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
+        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 sm:bg-inherit">
 
             {{-- BRAND --}}
             <x-app-brand class="p-5 pt-3" />
@@ -64,7 +64,7 @@
 
     {{--  TOAST area --}}
     <x-toast />
-    
+
 </body>
 
 </html>
