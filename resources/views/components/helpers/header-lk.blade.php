8aae28ade1e9d0022d81d723432e7e435d7d06e6
GrCOTE7 <grcote7@gmail.com>
2024-11-19 00:41:11 +0100

    Feat: Lien FE in all admin pages

--- resources/views/components/helpers/header-lk.blade.php
+++ resources/views/components/helpers/header-lk.blade.php
@@ -1,24 +1,14 @@
-@props(['title', 'lk' => 'No link', 'dashboardBtn' => true])
+@props(['title', 'dashboardBtn' => true])
 
 <div>
-    - HELPER SS BTN
-    <a href="/" title="{{ __('Go on site') }}">
-        <x-header title="{{ __('Settings1 ') }}" separator progress-indicator />
-    </a>
-
-
-    <x-header :title="__($title).'777'" separator progress-indicator>
-        <x-slot:actions>
+    <a href="{{ route('home') }}"" title="{{ __('Go on site') }}">
+        <x-header :title="__($title)" separator progress-indicator>
             @if ($dashboardBtn)
-                <x-button icon="s-building-office-2" :label="__('Dashboard')" class="btn-outline lg:hidden" :link="$lk" />
+                <x-slot:actions>
+                    <x-button icon="s-building-office-2" :label="__('Dashboard')" class="btn-outline lg:hidden"
+                        link="{{ route('admin') }}" />
+                </x-slot:actions>
             @endif
-        </x-slot:actions>
-    </x-header>
-
-    <x-header title="{{ __('Settings ORI') }}" separator progress-indicator>
-        <x-slot:actions>
-            <x-button icon="s-building-office-2" label="{{ __('Dashboard') }}" class="btn-outline lg:hidden"
-                link="{{ route('admin') }}" />
-        </x-slot:actions>
-    </x-header>
+        </x-header>
+    </a>
 </div>
